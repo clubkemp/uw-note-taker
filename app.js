b50a943691e95175ee027bc876e3e0d8afc47f97
Jonny K <Kempj2.jk@gmail.com>
2020-10-07 20:18:07 -0700

    routes for serving html and for delivering json in place

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require('express')
+const fs = require('fs');
+const path = require("path");
 
 
 const app = express();
-const PORT = 2020
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/notes', (req, res) =>{
+    console.log("notes")
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+app.get('/api/notes', (req, res) =>{
+    const json = JSON.parse(fs.readFileSync("./db/db.json"))
+    res.json(json)
+})
+
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+
+
 
 //TODO: routes needed
     //GET /notes - returns the notes.html file
